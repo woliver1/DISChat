9ace21a35e5349e74de76a0b3684bed2b78c2252
Will Oliver <woliver1@gmail.com>
2016-02-17 22:31:35 +1100

    Comments cleaned up.

--- src/main/java/woliver/dischat/WillEID.java
+++ src/main/java/woliver/dischat/WillEID.java
@@ -1,20 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * WillEID
+ *
+ * Version 0.1
+ * 17/2/16
+ *
+ * Copyright (c) 2015, William Oliver. All rights reserved.
+ *
+ * This work is licensed under the GPL Version 3 open source license, available
+ * at http://www.gnu.org/licenses/
  */
 package woliver.dischat;
 
 import edu.nps.moves.dis.EntityID;
 
 /**
- *
- * Copyright (c) 2015, William Oliver. All rights reserved.
- *
- * This work is licensed under the GPL Version 3 open source license, available
- * at http://www.gnu.org/licenses/
- *
- * @author William Oliver
+ * Holds a DIS entity identifier
+ * 
+ * The OpenDIS provided EntityID class does not have a sensible toString
+ * method, this class simply extends it to have a toString that outputs the DIS
+ * entity id in a more usual (for DIS) format.
+ * @author woliver
  * @version 0.1
  */
 public class WillEID extends EntityID {
